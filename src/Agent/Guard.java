2756f1d2f388d8983f03fb2a73fd6df434ce110a
thibtd <35340842+thibtd@users.noreply.github.com>
2019-03-13 12:12:23 +0100

    random move for guard

--- src/Agent/Guard.java
+++ src/Agent/Guard.java
@@ -1,18 +1,22 @@
 package Agent;
 import World.WorldMap;
 import java.awt.geom.Point2D;
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 
-/**
- * A subclass of Agent for the Intruders
- * @author Benjamin
- */
 
 public class Guard extends Agent {
 
     /**
      * A subclass of Agent for the Guards with an internal map containing the starting positions of other guards and the terrain across the map
-     * @author Benjamin
+     * @author Benjamin, Thibaut
      */
+
+    double speed = 1.4;
+    int[] visualRange = new int[2];
+    double viewingAngle;
+
 
     public Guard(Point2D.Double position, double direction)
     {
@@ -28,5 +32,37 @@ public class Guard extends Agent {
         Guard o = (Guard) obj;
         if((o.direction == this.direction) && (o.position.equals(this.position))) equals = true;
         return equals;
+    }
+    public int[] getVisualRange(){
+        if (coordinatesToCell(position) == 5){ // i.e. guard in on a tower
+           visualRange[0] = 2;
+           visualRange[1] = 15;
+           viewingAngle = 30;
+        }
+        else {
+            visualRange[0] = 0;
+            visualRange[1] = 6;
+            viewingAngle = 45;
+        }
+        return visualRange;
+    }
+    public void openTower()
+    {
+        if (coordinatesToCell(position) == 5)
+        {
+            class OpenTower extends TimerTask
+            {
+                public void run()
+                {
+                    //worldMap.updateTile((int)position.getX(), (int)position.getY(), 5);
+                    knownTerrain[(int)position.getX()][(int)position.getY()] = 5;
+                }
+            }
+            Timer timer = new Timer();
+            TimerTask openTower = new OpenTower();
+
+                timer.schedule(openTower, 3000);
+            timer.cancel();
+        }
     }
 }
