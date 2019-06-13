f20064173bbe213d324a7aec4b02cf176144ce57
Takowski <takowski@outlook.com>
2019-06-13 13:51:24 +0200

    ignore test file

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Test {
     public static void main(String[] args) {
         System.out.println("test");
         System.out.println(-370%360);
+        ArrayList<Double> test = new ArrayList<Double>();
+        test.add((Double)5.0);
+
     }
 }
